8d6269a2329d11d7b7c46d209ed8ec3d7a73d6ac
Andree Klattenhoff <mail@andr.ee>
2020-11-16 22:38:46 +0100

    fix screen/i3 init

--- scripts/screens.sh
+++ scripts/screens.sh
@@ -22,26 +22,9 @@ if [[ "$HOSTNAME" == "pico" ]]; then
       fi
     done
   fi
-  #exec --no-startup-id DISPLAY=":0" /usr/bin/picom -CGb
-  exec --no-startup-id /usr/bin/feh --bg-scale $HOME/docs/misc/wallpaper.png
-  exec --no-startup-id /usr/bin/twmnd
-  exec --no-startup-id /usr/bin/xbindkeys
-  exec --no-startup-id /usr/bin/nm-applet
-  exec --no-startup-id /usr/bin/clipit
-  exec --no-startup-id /usr/bin/pa-applet
-  exec --no-startup-id /usr/bin/blueman-applet
-  #exec --no-startup-id /usr/local/bin/stretchly
-elif [[ "$HOSTNAME" == "desktop" ]]; then
-  exec --no-startup-id /usr/bin/feh --bg-scale $HOME/dev/linux/Andreelec.tv/distributions/custom/splash/splash-1080.png
-  exec --no-startup-id /usr/bin/twmnd
-  exec --no-startup-id /usr/bin/xbindkeys
-  exec --no-startup-id /usr/bin/clipit
-  exec --no-startup-id /usr/bin/pa-applet
-#  exec --no-startup-id /usr/bin/xss-lock i3lock -c 000
-#  exec --no-startup-id /usr/bin/xset -display ":0.0" dpms 300 600 1200
 fi
 
 sleep 0.3
 
-~/.config/polybar/init.sh
+source $HOME/.config/polybar/init.sh
 
