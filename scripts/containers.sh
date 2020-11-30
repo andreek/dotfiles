41befd50cd499499de50e817494858c339d47d11
Andree Klattenhoff <mail@andr.ee>
2020-11-30 21:28:07 +0100

    fix podman info

--- scripts/containers.sh
+++ scripts/containers.sh
@@ -3,4 +3,6 @@ containerCount=$(podman ps --format "{{.Names}}" | wc -l)
 if [ $containerCount -gt 0 ]; then
   names=$(podman ps --sort names --format "{{.Names}}" | awk -vORS=' • ' '{ print $1 }' | sed 's/ • $/\n/')
   echo $names
+else
+  exit 1
 fi
