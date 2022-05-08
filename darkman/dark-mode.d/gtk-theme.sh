dda11757f0dd30e562789c7bfdb7af56c73eb0fe
Andree Klattenhoff <mail@andr.ee>
2022-05-08 14:29:00 +0200

    [darkman] Change gtk2, gtk3 and qt5 settings as well

--- darkman/dark-mode.d/gtk-theme.sh
+++ darkman/dark-mode.d/gtk-theme.sh
@@ -1,2 +1,5 @@
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+sed -i 's/"Arc-Darker"/"Arc-Dark"/g' $HOME/.gtkrc-2.0
+sed -i 's/Arc-Darker/Arc-Dark/g' $HOME/.config/gtk-3.0/settings.ini
+sed -i 's/Adwaita/Adwaita-Dark/g' $HOME/.config/qt5ct/qt5ct.conf
--- darkman/light-mode.d/gtk-theme.sh
+++ darkman/light-mode.d/gtk-theme.sh
@@ -1,2 +1,5 @@
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Darker
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
+sed -i 's/"Arc-Dark"/"Arc-Darker"/g' $HOME/.gtkrc-2.0
+sed -i 's/Arc-Dark/Arc-Darker/g' $HOME/.config/gtk-3.0/settings.ini
+sed -i 's/Adwaita-Dark/Adwaita/g' $HOME/.config/qt5ct/qt5ct.conf
