8c9f2b602b6c5733239d0b4cacdb38f64484bdb3
Andree Klattenhoff <mail@andr.ee>
2022-06-06 15:55:45 +0200

    Fix bug in darkman scripts

--- darkman/dark-mode.d/gtk-theme.sh
+++ darkman/dark-mode.d/gtk-theme.sh
@@ -1,5 +1,6 @@
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 sed -i 's/"Arc-Darker"/"Arc-Dark"/g' $HOME/.dotfiles/.config/.gtkrc-2.0
-sed -i 's/Arc-Darker/Arc-Dark/g' $HOME/.dotfiles/.config/gtk-3.0/settings.ini
-sed -i 's/Adwaita/Adwaita-Dark/g' $HOME/.dotfiles/.config/qt5ct/qt5ct.conf
+sed -i 's/gtk-icon-theme-name = Arc-Darker/gtk-icon-theme-name = Arc-Dark/g' $HOME/.dotfiles/.config/gtk-3.0/settings.ini
+sed -i 's/gtk-theme-name = Arc-Darker/gtk-theme-name = Arc-Dark/g' $HOME/.dotfiles/.config/gtk-3.0/settings.ini
+sed -i 's/style=Adwaita/style=Adwaita-Dark/g' $HOME/.dotfiles/.config/qt5ct/qt5ct.conf
--- darkman/light-mode.d/gtk-theme.sh
+++ darkman/light-mode.d/gtk-theme.sh
@@ -1,5 +1,6 @@
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Darker
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 sed -i 's/"Arc-Dark"/"Arc-Darker"/g' $HOME/.dotfiles/.config/.gtkrc-2.0
-sed -i 's/Arc-Dark/Arc-Darker/g' $HOME/.dotfiles/.config/gtk-3.0/settings.ini
-sed -i 's/Adwaita-Dark/Adwaita/g' $HOME/.dotfiles/.config/qt5ct/qt5ct.conf
+sed -i 's/gtk-icon-theme-name = Arc-Dark/gtk-icon-theme-name = Arc-Darker/g' $HOME/.dotfiles/.config/gtk-3.0/settings.ini
+sed -i 's/gtk-theme-name = Arc-Dark/gtk-theme-name = Arc-Darker/g' $HOME/.dotfiles/.config/gtk-3.0/settings.ini
+sed -i 's/style=Adwaita-Dark/style=Adwaita/g' $HOME/.dotfiles/.config/qt5ct/qt5ct.conf
