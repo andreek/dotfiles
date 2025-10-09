c33feb93e0b47366d2ba9f52cc409b3771e05544
Andree Klattenhoff <mail@andr.ee>
2025-10-09 22:26:24 +0200

    darkman: Add shebang line

--- darkman/dark-mode.d/gtk-theme.sh
+++ darkman/dark-mode.d/gtk-theme.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sed -i 's/style=Adwaita/style=Adwaita-Dark/g' "HOME/.dotfiles/.config/qt5ct/qt5ct.conf"
 /usr/bin/gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
 /usr/bin/gsettings set org.gnome.desktop.interface color-scheme prefer-dark
--- darkman/light-mode.d/gtk-theme.sh
+++ darkman/light-mode.d/gtk-theme.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sed -i 's/style=Adwaita-Dark/style=Adwaita/g' "$HOME/.dotfiles/.config/qt5ct/qt5ct.conf"
 /usr/bin/gsettings set org.gnome.desktop.interface gtk-theme Arc-Daker
 /usr/bin/gsettings set org.gnome.desktop.interface color-scheme prefer-light
